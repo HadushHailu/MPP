8afcaecadc3f28d75180412a1ce81d118dd4cec7
Hadush <h.hailu@qibitech.com>
2024-04-10 06:24:20 -0500

    Add project

---
+++ src/lesson5/exercise/Library.java
@@ -0,0 +1,8 @@
+package lesson5.exercise;
+
+import javax.swing.*;
+
+public class Library {
+    private JButton button1;
+    private JButton button2;
+}
---
+++ src/lesson5/exercise/lib.java
@@ -0,0 +1,8 @@
+package lesson5.exercise;
+
+import javax.swing.*;
+
+public class lib {
+    private JLabel Login;
+    private JTable table1;
+}
