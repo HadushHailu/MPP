1818e860b8e1c49c0b42ca5a85c2551950d0fb77
Luwam Weldetensae <luwamweldetensae@Luwams-MacBook-Pro.local>
2024-04-15 18:57:03 -0500

    prob3 solution added

--- src/lesson7/labs/prob3/ForEachExample.java
+++ src/lesson7/labs/prob3/ForEachExample.java
@@ -3,6 +3,7 @@ package lesson7.labs.prob3;
 import java.util.Arrays;
 import java.util.List;
 import java.util.TreeSet;
+import java.util.function.Consumer;
 
 
 public class ForEachExample {
@@ -10,14 +11,24 @@ public class ForEachExample {
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", 
 				"Away", "On Vacation", "Everywhere you want to be");
-		
+
+
 		//print each element of the list in upper case format
-		
+		System.out.println("------using lambda------");
+		list.forEach(l-> System.out.println(l.toUpperCase()));
+
+		//implement a Consumer
+		System.out.println("------Pre Java 8, using new forEach method------");
+		Consumer<String> consumer = new Consumer<String>() {
+			@Override
+			public void accept(String str) {
+				System.out.println(str.toUpperCase());
+			}
+		};
+		list.forEach(consumer);
 		
 		
 	}
-	
-	//implement a Consumer
 	
 	
 }
