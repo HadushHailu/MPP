67cc1f4b6137cd6cf9145f3464a7d6baa145f15e
sanjuktasunar <misshean5@gmail.com>
2024-04-16 14:43:01 -0500

    lab 8 problem1 solution

--- src/lesson7/lecture/defaultmethods/NameAddress.java
+++ src/lesson7/lecture/defaultmethods/NameAddress.java
@@ -1,5 +1,8 @@
 package lesson7.lecture.defaultmethods;
 
+import java.util.Collection;
+import java.util.Collections;
+
 public interface NameAddress {
 	String getFirstName();
 	String getLastName();
@@ -16,4 +19,36 @@ public interface NameAddress {
 		return getFullName() + "\n" + getFullAddress();
 	}
 	
+}
+class Test implements NameAddress{
+	public static void main(String[] args) {
+		var d=new Test();
+//		Collection
+		//d.getFullName();
+	}
+
+	@Override
+	public String getFirstName() {
+		return null;
+	}
+
+	@Override
+	public String getLastName() {
+		return null;
+	}
+
+	@Override
+	public String getStreet() {
+		return null;
+	}
+
+	@Override
+	public String getCity() {
+		return null;
+	}
+
+	@Override
+	public String getState() {
+		return null;
+	}
 }
